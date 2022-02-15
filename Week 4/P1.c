9765ed2722e93f4e0e084d0f1a256d537b78e8fa
Renita1206 <bluecoder1206@gmail.com>
2022-02-15 14:30:56 +0530

    W4

---
+++ Week 4/P1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+int main()
+{
+	char write_msg[BUFFER_SIZE] = "Greetings";
+	char read_msg[BUFFER_SIZE] = "";
+	int fd[2];
+	pid_t pid;
+	
+	if(pipe(fd)==-1)
+	{
+		printf("Pipe failed\n");
+		return 1;
+	}
+	
+	pid = fork();
+	
+	if(pid>0) 	//parent process
+	{
+		close(fd[READ_END]);
+		write(fd[WRITE_END],write_msg, strlen(write_msg)+1);
+		close(fd[WRITE_END]);
+		wait(NULL);
+	}
+	else if(pid == 0) // child process
+	{
+		close(fd[WRITE_END]);
+		read(fd[READ_END], read_msg, BUFFER_SIZE);
+		printf("Child process read - %s\n", read_msg);
+		close(fd[READ_END]);
+	}
+	else
+	{
+		printf("Fork failed\n");
+		return 1;
+	}
+}
+	
+	
+	
